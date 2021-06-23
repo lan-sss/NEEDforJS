a3b4ca968d15a86c8a422e177ce2f16cfb634fbc
lan-sss <84177584+lan-sss@users.noreply.github.com>
2021-06-23 11:05:35 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
-  gameArea = document.querySelector('gameArea'),
+  gameArea = document.querySelector('.gameArea'),
   car = document.createElement('div'); //элмент добавлен в функцию startGame
 
 car.classList.add('car'); //добавили класс через метод add, добавили стилей в css
@@ -12,8 +12,8 @@ start.onclick = function() {
 };
 */
 
-start.addEventListener('click', startGame)
-document.addEventListener('keydown', startRun) 
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun); 
 document.addEventListener('keyup', stopRun);
 
 //click срабатывает, когда нажимаем левую кнопку мыши, запускается функция startGame, см. ее ниже
@@ -42,7 +42,7 @@ function startGame(){
   setting.start = true; // изменяем объект setting и его свойство start
   gameArea.appendChild(car);  //appendChild вставить ребенка, т.е. объект автомобиля
   requestAnimationFrame(playGame); //анимируем функцию playGame
-};
+}
  
 
 /* это как было, потом изменили, см. ниже
@@ -61,26 +61,36 @@ function playGame(){
   if (setting.start){
     requestAnimationFrame(playGame);
   }
-};
-//requestAnimationFrame(playGame) чтобы игра не останавливалась и чтобы это было плавно, это рекурсия, когда функция запускает сама себя? и это прописано в условии пока игра запущена, т.е. start true. изменили if (setting.start === true), т.к. нам не нужно чтобы всегда соответсоввало true
+}
+/*requestAnimationFrame(playGame) чтобы игра не останавливалась,
+ и чтобы это было плавно, это рекурсия, когда функция запускает сама себя?
+ и это прописано в условии пока игра запущена, т.е. start true. изменили 
+ if (setting.start === true), т.к. нам не нужно чтобы всегда соответсоввало true*/
 
 //нажатие клавиш
 
 function startRun(event){
   event.preventDefault(); 
   keys[event.key] = true; 
-};
+}
 
 function stopRun(event) {
   event.preventDefault();  
   keys[event.key] = false; 
-};
+}
 
-//добавив event в скобки вместо 'start' мы выводим событие в консоль со многими параметрами
-//event.preventDefault() так мы отменим стандартные настройки браузера, уберем скроллинг у стрелок на клавиатуре, вообще мы полностью убрали стандартное поведение для клавиш
-// добавив .key в выражение console.log(event.key) мы видим какую клавишу нажали, но при этом больше не видим события event
-// удалили   console.log(event.key)
-//keys[event.key] = true обратились к объекту keys и свойству объекта event.key , чтобы при нажатии стрелок название нажатой клавиши передалось в функцию startRun и мы мгди присвоить новое значение true, таким образом наш скрипт будет видеть, что ArrowRight и т.д. true и будет двигать автомобиль в нужную сторону, в тот момент когда отпускаем клавишу, мы возвращаем значение false в функции stopRun
+/*
+добавив event в скобки вместо 'start' мы выводим событие в консоль со многими параметрами
+event.preventDefault() так мы отменим стандартные настройки браузера, 
+уберем скроллинг у стрелок на клавиатуре, вообще мы полностью убрали стандартное поведение для клавиш
+добавив .key в выражение console.log(event.key) мы видим какую клавишу нажали, но при этом больше не видим события event
+удалили   console.log(event.key)
+keys[event.key] = true обратились к объекту keys и свойству объекта event.key , 
+чтобы при нажатии стрелок название нажатой клавиши передалось в функцию startRun 
+и мы мгди присвоить новое значение true, таким образом наш скрипт будет видеть, 
+что ArrowRight и т.д. true и будет двигать автомобиль в нужную сторону, 
+в тот момент когда отпускаем клавишу, мы возвращаем значение false в функции stopRun
+*/
 
 
 
