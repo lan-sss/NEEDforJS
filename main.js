d8ae6be2eef2fe5efab46490a663abffd4c08e40
lan-sss <84177584+lan-sss@users.noreply.github.com>
2021-06-24 16:09:58 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-//нам нужно получить 3 элемента: score, start и gameArea, берем из html классы
-const score = document.querySelector('.score'),//document это объект с элментами страницы
+const MAX_ENEMY = 7;
+const music = new Audio('audio.mp3');
+const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea'),
-  car = document.createElement('div'); //элмент добавлен в функцию startGame
-
-car.classList.add('car'); //добавили класс через метод add, добавили стилей в css
-
-/* 
-метод устарел, см. ниже, используй addEventListener
-start.onclick = function() {
-  start.classList.add('hide');
-};
-*/
+  car = document.createElement('div'); 
+  car.classList.add('car'); 
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun); 
 document.addEventListener('keyup', stopRun);
 
-//click срабатывает, когда нажимаем левую кнопку мыши, запускается функция startGame, см. ее ниже
-//keydown срабатывает, когда нажата ЛЮБАЯ клавиша, запускается функция startRun, см. ее ниже
-//keyup срабатывает, когда отпускаем РАНЕЕ НАЖАТУЮ клавишу, запускается функция stopRun, см. ее ниже
-
-
-//создадим объект при нажатии клавиш стрелок на клавиатуре
 const keys = {
   ArrowUp: false,
   ArrowDown: false,
@@ -30,151 +17,187 @@ const keys = {
   ArrowLeft: false
 };
 
-// объект, содержащий свойства: кол-во очков, скорость игры, статуса (запущена или нет), трафик (сложность)
 const setting = {
   start: false,
   score: 0,
-  speed: 5,
+  speed: 7,
   traffic: 3
 };
 
 function getQuantityElements(heightElement) { 
-  //эта функция вычислит ск-ко элементов поместится на странице с этой высотой
-  //вычисляем высоту страницы, и с помощью return будет возвращаться конкретное значение
   return document.documentElement.clientHeight / heightElement + 1;
 }
 
+const getRandomEnemy = (max) => Math.floor((Math.random() * max) + 1);
+
 function startGame() {
-  start.classList.add('hide'); //start.classList.add('hide') связано с css, см. класс hide в css
-  // напишем цикл для линий, см ниже
+  gameArea.innerHTML = '';
+  start.classList.add('hide'); 
+  music.play();
+  gameArea.style.minHeight = 100 + 'vh';
   for (let i = 0; i < getQuantityElements(100); i++) {
-    const line = document.createElement('div'); // создаем линию, каждый цикл будет создаваться 1 линия
-    line.classList.add('line'); //добавили класс, у которого сили прописаны в css
-    line.style.top = (i * 100) + 'px'; //75 это расстояние между линиями
-    line.y = i * 100; // чтобы линии начали двигаться, продолжение в playGame = > moveRoad
-    gameArea.appendChild(line); //расположили линии на экране
+    const line = document.createElement('div'); 
+    line.classList.add('line'); 
+    line.style.top = (i * 100) + 'px'; 
+    line.y = i * 100; 
+    gameArea.appendChild(line); 
   }
 
-  //создаем автомобили соперники
   for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++ ) {  
-    // * setting.traffic это расстояние между автомобилями
-    const enemy = document.createElement('div'); //создаем автомобиль
-    enemy.classList.add('enemy'); //добавили класс
+    const enemy = document.createElement('div'); 
+    enemy.classList.add('enemy'); 
     enemy.y = -100 * setting.traffic * (i + 1); 
-    //100 это высота автомобиля, см. в css, минус, чтобы авто были выше дороги (сверху)
-    //setting.traffic это число которе влияет на плотность, 
-    //i чтобы авто располагались на каком-то расстоянии друг от друга, но i первое это 0, тогда все равно 0, поэтому +1
     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) +'px';
-    // где по горизонтали будут располагаться авто, Math.random()случайное чсило, Math.floor - округление
-    enemy.style.top = enemy.y + 'px'; // расстояние от верха игрового пространства, мы его вычисляли с помощью у
-    enemy.style.background = 'transparent url(./image/enemy_1_bugatti.png) center / cover no-repeat';
-    gameArea.appendChild(enemy); //расположили авто на игровой арене
-
-
+    enemy.style.top = enemy.y + 'px';
+    enemy.style.background = `
+      transparent 
+      url(./image/enemy${getRandomEnemy(MAX_ENEMY)}.png) 
+      center / cover 
+      no-repeat`;
+    gameArea.appendChild(enemy); 
   }
 
+  for (let i = 0; i < 1; i++) {
+    const green = document.createElement('div');  
+    green.classList.add('green'); 
+    green.style.top = (i * 500) + 'px'; 
+    green.y = i * 100;
+    gameArea.appendChild(green); 
+  }
 
-  setting.start = true; // изменяем объект setting и его свойство start
-  gameArea.appendChild(car);  //appendChild вставить ребенка, т.е. объект автомобиля
+  for (let i = 0; i < 1; i++) {
+    const green1 = document.createElement('div');  
+    green1.classList.add('green1'); 
+    green1.style.top = (i * 500) + 'px'; 
+    green1.y = i * 100;
+    gameArea.appendChild(green1); 
+  }
+
+for (let i = 0; i < 1; i++) {
+    const green2 = document.createElement('div');  
+    green2.classList.add('green2'); 
+    green2.style.top = (i * 500) + 'px'; 
+    green2.y = i * 100;
+    gameArea.appendChild(green2); 
+  }
+
+  setting.score = 0;
+  setting.start = true; 
+  gameArea.appendChild(car);  
+  car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+  car.style.top = 'auto'; 
+  car.style.bottom = '10px'; 
   setting.x = car.offsetLeft; 
-  //добавили свойство х в объект setting для движения влево-вправо, offsetLeft-свойство left в css у car
   setting.y = car.offsetTop;
-  //добавили свойство y в объект setting для движения вверх-вниз
-  requestAnimationFrame(playGame); //анимируем функцию playGame
+  requestAnimationFrame(playGame); 
 }
  
-
-/* это как было, потом изменили, см. ниже
-function startRun(start) {
-  console.log('start');
-}
-function stopRun(stop) {
-  console.log('stop');
-}*/
-
-//анимация
 function playGame(){
   if (setting.start) {
+    setting.score +=setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
-    if(keys.ArrowLeft && setting.x > 0) { //&& setting.x > 0 чтобы машина не выходила за пределы поля 
-      setting.x -=setting.speed;  //движение в лево по оси х с учетом скорости, если зажата клавиша ArrowLeft
-    }
-    if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) { 
-      //&& setting.x < (gameArea.offsetWidth - 50) значит выполняется при условии => ширина поля минус ширина машины
-      setting.x +=setting.speed;  //движение вправо по оси х с учетом скорости, если зажата клавиша ArrowRight
-    }
-    if (keys.ArrowUp && setting.y > 0) {
-      setting.y -=setting.speed; //движение вверх по оси у с учетом скорости, если зажата клавиша ArrowUp
-    }
-    if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
-      setting.y +=setting.speed; //движение вниз по оси у с учетом скорости, если зажата клавиша ArrowDown
-    }
+    moveGreen();
+    moveGreen1();
+    moveGreen2();
+  if(keys.ArrowLeft && setting.x > 0) {
+    setting.x -=setting.speed; 
+  }
+   if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) { 
+     setting.x +=setting.speed;
+   }
+  if (keys.ArrowUp && setting.y > 0) {
+     setting.y -=setting.speed; 
+  }
+  if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+     setting.y +=setting.speed; 
+  }
 
-    car.style.left = setting.x + 'px'; 
-    //чтобы сработало действие нажатий клавиш (см. выше), передаем стиль left на страницу и меняем на setting.x
-    car.style.top = setting.y + 'px'; 
-
-    requestAnimationFrame(playGame);
+  car.style.left = setting.x + 'px'; 
+  car.style.top = setting.y + 'px'; 
+  requestAnimationFrame(playGame);
   }
 }
-/*requestAnimationFrame(playGame) чтобы игра не останавливалась,
- и чтобы это было плавно, это рекурсия, когда функция запускает сама себя?
- и это прописано в условии пока игра запущена, т.е. start true. изменили 
- if (setting.start === true), т.к. нам не нужно чтобы всегда соответсоввало true*/
-
-//нажатие клавиш
 
 function startRun(event){
-  event.preventDefault(); 
-  keys[event.key] = true; 
+  if (keys.hasOwnProperty(event.key)) { 
+    event.preventDefault(); 
+    keys[event.key] = true; 
+  } 
 }
 
 function stopRun(event) {
-  event.preventDefault();  
-  keys[event.key] = false; 
+  if (keys.hasOwnProperty(event.key)) {
+    event.preventDefault();  
+    keys[event.key] = false; 
+  }
 }
 
-/*
-добавив event в скобки вместо 'start' мы выводим событие в консоль со многими параметрами
-event.preventDefault() так мы отменим стандартные настройки браузера, 
-уберем скроллинг у стрелок на клавиатуре, вообще мы полностью убрали стандартное поведение для клавиш
-добавив .key в выражение console.log(event.key) мы видим какую клавишу нажали, но при этом больше не видим события event
-удалили   console.log(event.key)
-keys[event.key] = true обратились к объекту keys и свойству объекта event.key , 
-чтобы при нажатии стрелок название нажатой клавиши передалось в функцию startRun 
-и мы мгди присвоить новое значение true, таким образом наш скрипт будет видеть, 
-что ArrowRight и т.д. true и будет двигать автомобиль в нужную сторону, 
-в тот момент когда отпускаем клавишу, мы возвращаем значение false в функции stopRun
-*/
-
 function moveRoad() {
-  let lines = document.querySelectorAll('.line'); //получили все линии с классом line и сохранили в переменную lines
+  let lines = document.querySelectorAll('.line'); 
   lines.forEach(function(line){
-    // нужно перебрать все элменты, эта функция будет запускатьс ястолько аз сколько у нас элментов, 
-    //line это элмент, который перебирается
-    //i это индекс элмента в нашем случае от 0 до 19, см цикл for, но он нас не нужен. 
    line.y +=setting.speed;
    line.style.top = line.y + 'px';
-  
     if(line.y > document.documentElement.clientHeight) { 
     line.y = -100;
-    //мы получили высоту страницы и когда линия будет уезжать вниз, мы будем поднимать ее вверх
-
   }
-
 });
 }
 
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
-  enemy.forEach(function(item){ //здесь записали item, а не enemy, чтобы не попасть на переменные
-    item.y +=setting.speed / 2;
-    item.style.top = item.y + 'px';
-//чтобы авто enemy после того как уехала вниз возвращалась обратно наверх пропишем условие
-if (item.y > document.documentElement.clientHeight) {
-  item.y = -100 * (setting.traffic + 2); 
-  item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) +'px';
-}
+  enemy.forEach(function(itemEnemy){ 
+  let carRect = car.getBoundingClientRect(); 
+  let enemyRect = itemEnemy.getBoundingClientRect(); 
+  if (carRect.top <= enemyRect.bottom && 
+    carRect.bottom >= enemyRect.top && 
+    carRect.right >= enemyRect.left && 
+    carRect.left <= enemyRect.right) { 
+      setting.start = false;
+      console.warn('ДТП');
+      start.classList.remove('hide'); 
+      score.style.top = start.offsetHeight; 
+  }
+    itemEnemy.y +=setting.speed / 2;
+    itemEnemy.style.top = itemEnemy.y + 'px';
+  if (itemEnemy.y > document.documentElement.clientHeight) {
+    itemEnemy.y = -100 * (setting.traffic+2); 
+    itemEnemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) +'px';
+  }
 });
 }
+
+function moveGreen() {
+  let greens = document.querySelectorAll('.green');
+  greens.forEach(function(itemGreen){
+    itemGreen.y +=setting.speed;
+    itemGreen.style.top = itemGreen.y + 'px';
+    if(itemGreen.y > document.documentElement.clientHeight) { 
+      itemGreen.y = -100;
+    }
+  });
+}
+
+function moveGreen1() {
+  let greens1 = document.querySelectorAll('.green1');
+  greens1.forEach(function(itemGreen1){
+   itemGreen1.y +=setting.speed;
+   itemGreen1.style.top = itemGreen1.y + 'px';
+    if(itemGreen1.y > document.documentElement.clientHeight) { 
+      itemGreen1.y = -100;
+    }
+  });
+}
+
+function moveGreen2() {
+  let greens2 = document.querySelectorAll('.green2');
+  greens2.forEach(function(itemGreen2){
+   itemGreen2.y +=setting.speed;
+   itemGreen2.style.top = itemGreen2.y + 'px';
+    if(itemGreen2.y > document.documentElement.clientHeight) { 
+      itemGreen2.y = -100;
+    }
+  });
+}
+
